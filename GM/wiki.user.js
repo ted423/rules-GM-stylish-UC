e807850e50e5565d9616a743ca09fc2907cdc0b6
Priston <priston910423@outlook.com>
2019-03-17 17:43:46 +0800

    域名更换

--- GM/wiki.user.js
+++ GM/wiki.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name		wiki open image
-// @version		2.2
+// @version		2.3
 // @description	wiki
 // @include		https://*.wikia.com/wiki/*
+// @include		https://*.fandom.com/wiki/*
 // @include		https://*.wikia.nocookie.net/*
 // @grant		none
 // @run-at		document-end
 // ==/UserScript==
 
-var temp=/^https?:\/\/.*\.wikia\.com\/wiki\/file/i;
-if(temp.test(location.href))
-	temp=document.getElementsByClassName("internal")[0];
-a=temp.href.split("/revision");
+var temp=/^https?:\/\/.*\.(wikia|fandom)\.com\/wiki\/file/i;
+if(temp.test(location.href)) temp=document.getElementsByClassName("internal")[0];
+var a = temp.href.split("/revision");
 temp.href=a[0];
 temp.click();
