ec8d2a0224d961040fc6f32e2c4499aeb26af44d
Priston <priston910423@outlook.com>
2019-03-23 12:33:18 +0800

    修正

--- GM/lofter.user.js
+++ GM/lofter.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name         Lofter显示原图
-// @namespace    https://github.com/ted423
-// @version      1.0
-// @description  Lofter显示原图
-// @author       ted423
-// @match        *://*.lofter.com/post/*
+// @name		Lofter显示原图
+// @namespace	https://github.com/ted423
+// @version		1.0
+// @description	Lofter显示原图
+// @grant		unsafeWindow
+// @author		ted423
+// @match		*://*.lofter.com/post/*
 // @run-at		document-end
 // ==/UserScript==
-(function() {
     "use strict";
-	var imgs=document.querySelectorAll(".img img");
+	var imgs = document.querySelectorAll(".img img");
 	[].forEach.call(imgs, function(img) {
 	img.src=img.src.split("?")[0];
 	console.log(img.src);
 	});
-})
