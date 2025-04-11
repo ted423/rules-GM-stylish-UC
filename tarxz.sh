bd4ed9939acf5d91fbd5d7793489d22e631e42c7
unknown <priston910423@gmail.com>
2025-04-11 14:15:42 +0800

    refactor: tarxz.sh support para

--- tarxz.sh
+++ tarxz.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-target_dir=$(dirname "$1")
-target=$(basename "$1")
-XZ_OPT='-T 0' tar -cvJf $target.tar.xz  $1 
+if [ "$#" -gt 0 ]; then
+  # 获取最后一个参数,参数放在前面，要打包的路径放在最后
+  allarg="$*"
+  last_arg="${!#}"
+  target_dir=$(dirname "${!#}")
+  target=$(basename "${!#}")
+  suffix_to_remove=" $last_arg"
+  para="${allarg%$suffix_to_remove}"
+else
+  echo "参数放在前面，要打包的路径放在最后"
+fi
+#echo "para is $para"
+#echo "XZ_OPT='-T 0' tar -C \"$target_dir\" $para -cvJf $target.tar.xz $target"
+XZ_OPT='-T 0' tar -C "$target_dir" $para -cvJf $target.tar.xz $target
