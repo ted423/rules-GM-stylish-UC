964038d50323ac30f97b45ec40868aed5e28f719
ted423 <priston910423@gmail.com>
2025-04-28 11:58:50 +0800

    perf: no need to get os ver

--- getPCmonitorSN.bat
+++ getPCmonitorSN.bat
@@ -3,21 +3,7 @@ setlocal enabledelayedexpansion
 chcp 65001 >nul
 REM bat default is ANSI(gbk) set to utf-8
 
-
-REM 版本号 10.0.XXXXX.YY
-for /f "tokens=2 delims=[]" %%a in ('ver') do (
-  for /f "tokens=2 delims= " %%b in ("%%a") do (
-   set "version_str=%%b"
-  )
-)
-REM echo !version_str!
-for /f "tokens=1 delims=. " %%c in ("%version_str%") do (
-    set "os_num=%%c"
-)
-REM echo !os_num!
-for /f "tokens=3 delims=. " %%d in ("%version_str%") do (
-  set "build_num=%%d"
-)
+echo 脚本很可能需要管理员权限才能获取显示器SN
 
 where pwsh >nul 2>nul
 if %errorlevel% equ 0 (
@@ -27,15 +13,6 @@ if %errorlevel% equ 0 (
     "$PCInfo = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber;Write-Host 'PC Serial Number:' $PCInfo; $monitorInfo = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID; if ($monitorInfo) { foreach ($monitor in $monitorInfo) { $serialNumber = [System.Text.Encoding]::ASCII.GetString($monitor.SerialNumberID); if ($serialNumber -ne 0 -and $serialNumber -ne 1) { Write-Host 'Monitor Serial Number:' $serialNumber; } } } else { Write-Host 'No monitor information found.'; };"
 ) else (
   set ps=powershell
-  if %os_num%==10 (
-    if %build_num% geq 22000 (
-      REM Win11
-      !ps! -NoProfile -ExecutionPolicy Bypass -Command "$PCInfo = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber;Write-Host 'PC Serial Number:' $PCInfo; $monitorInfo = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID; if ($monitorInfo) { foreach ($monitor in $monitorInfo) { $serialNumber = [System.Text.Encoding]::ASCII.GetString($monitor.SerialNumberID); if ($serialNumber -ne 0 -and $serialNumber -ne 1) { Write-Host 'Monitor Serial Number:' $serialNumber; } } } else { Write-Host 'No monitor information found.'; };"
-    ) else (
-      echo Win 10/Server 不装 PowerShell 7 无法获取显示器 SN
-      REM Win10 or Server 2019/2022
-      !ps! -NoProfile -ExecutionPolicy Bypass -Command "$PCInfo = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber;Write-Host 'PC Serial Number:' $PCInfo;"
-    )
-  )
+  !ps! -NoProfile -ExecutionPolicy Bypass -Command "$PCInfo = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber;Write-Host 'PC Serial Number:' $PCInfo; $monitorInfo = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorID; if ($monitorInfo) { foreach ($monitor in $monitorInfo) { $serialNumber = [System.Text.Encoding]::ASCII.GetString($monitor.SerialNumberID); if ($serialNumber -ne 0 -and $serialNumber -ne 1) { Write-Host 'Monitor Serial Number:' $serialNumber; } } } else { Write-Host 'No monitor information found.'; };"
 )
 pause
