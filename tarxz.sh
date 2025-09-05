5e85124987b313af1a7c7242bf3e250f704d3157
ted423 <priston910423@gmail.com>
2025-09-05 17:59:31 +0800

    Update tarxz.sh

--- tarxz.sh
+++ tarxz.sh
@@ -3,6 +3,10 @@ if [ "$#" -gt 0 ]; then
   # 获取最后一个参数,参数放在前面，要打包的路径放在最后
   allarg="$*"
   last_arg="${!#}"
+  if [[ "$last_arg" == "." || "$last_arg" == ".." ]]; then
+    echo "请勿打包 . .. 目录，会导致产生的压缩文件被打包，造成问题"
+    exit 1
+  fi
   target_dir=$(dirname "${!#}")
   target=$(basename "${!#}")
   suffix_to_remove=" $last_arg"
