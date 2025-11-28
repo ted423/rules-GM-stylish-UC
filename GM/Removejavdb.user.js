e233984d29f80f8d083fe959e14d995b2a3a752d
ted423 <priston910423@gmail.com>
2025-11-29 00:27:05 +0800

    Update Removejavdb.user.js

--- GM/Removejavdb.user.js
+++ GM/Removejavdb.user.js
@@ -3,9 +3,10 @@
 // @namespace   ted423
 // @include     https://javdb.com/v/*
 // @description 去除磁力链接里的"javdb.com"
-// @version     1.0
+// @version     1.1
 // @grant       none
+// @run-at      document-end
 // ==/UserScript==
 
-var temp=document.querySelectorAll('button.button.copy-to-clipboard');
-[].forEach.call(temp,function(each){each.dataset.clipboardText = each.dataset.clipboardText.replace('[javdb.com]','');})
+var temp=document.querySelectorAll('a[href*="magnet"]');
+[].forEach.call(temp,function(each){each.href = each.href.replace('[javdb.com]','');})
